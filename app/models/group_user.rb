3e2ba9f696d1e33b855cdaac0c1980768b3c3032
KerryAnn Harry <kerryann.harry@yahoo.com>
2021-02-18 09:56:06 -0600

    folders created: registration, sessions, users

--- app/models/group_user.rb
+++ app/models/group_user.rb
@@ -1,4 +1,5 @@
 class GroupUser < ActiveRecord::Base
     belongs_to :group 
-    belongs_to :user 
+    belongs_to :user
+    validates :group, :user, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
     has_many :group_users
     has_many :groups, through: :group_users
+    validates :username, :email, presence: true, unqueness: true 
+    has_secure_password
 end
