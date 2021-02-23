62881b4782f13aa7ff7004b1528a7f22af702c26
KerryAnn Harry <kerryann.harry@yahoo.com>
2021-02-23 13:55:57 -0600

    email and password length for users.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
     has_many :group_users
     has_many :groups, through: :group_users
-    validates :username, :email, presence: true, uniqueness: true 
+    validates :username, :email, presence: true, uniqueness: true, length {in: 5..20} 
     has_secure_password
+    validates :password, length {in: 8..50}
 end
