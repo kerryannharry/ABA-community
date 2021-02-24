75e337d8b9201b8cec0ebe192ee0c6422a58ac50
KerryAnn Harry <kerryann.harry@yahoo.com>
2021-02-24 15:53:51 -0600

    user validations with email and password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ActiveRecord::Base
     has_many :group_users
     has_many :groups, through: :group_users
-    validates :username, :email, presence: true, uniqueness: true, length {in: 5..20} 
+    validates :username, presence: true, uniqueness: true, length {in: 5..20} 
     has_secure_password
-    validates :password, length {in: 8..50}
+    validates :password, length {in: 8..50}, confirmation: true, on: :create
+    validates :email, presence: true, uniqueness: true format: {with: /(?<username>[^@\s]+)@/}
 end
