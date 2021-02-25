81b4e57aa2b9ae965b73bd6dc32e873b0fe2de28
KerryAnn Harry <kerryann.harry@yahoo.com>
2021-02-25 08:23:56 -0600

    session controller create: login/out added.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -35,27 +35,7 @@ class ApplicationController < Sinatra::Base
 erb:"users/show"
   end
 
-  get '/login'do 
-  redirect_if_logged_in
-    erb :"sessions/login"
-  end
-
-  post '/login' do
-    user = User.find_by_username(params[:username])
-    if user && user.authenticate(params[:password])
-      flash[:success]="You've successfully logged in!"
-      session[:user_id]= user.id 
-      redirect "/users/#{user.id}"
-    else
-      flash[:error]= "Invaild login information."
-      redirect "/login"
-    end
-  end
-
-  get '/logout' do
-  session.clear
-  redirect "/login"
-end
+  
 helpers do 
   def current_user
     @current_user ||= User.find_by_id(session[:user_id]) if session[:user_id]
---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,21 @@
+get '/login'do 
+  redirect_if_logged_in
+    erb :"sessions/login"
+  end
+
+  post '/login' do
+    user = User.find_by_username(params[:username])
+    if user && user.authenticate(params[:password])
+      flash[:success]="You've successfully logged in!"
+      session[:user_id]= user.id 
+      redirect "/users/#{user.id}"
+    else
+      flash[:error]= "Invaild login information."
+      redirect "/login"
+    end
+  end
+
+  get '/logout' do
+  session.clear
+  redirect "/login"
+  end
